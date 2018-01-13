f7801b377496ebe2b238d8978010311823769fc8
Srujan Barai <srujan_barai@yahoo.com>
2018-01-13 01:19:20 -0500

    Initial Commit

---
+++ app/src/main/java/com/example/sbarai/openkart/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.sbarai.openkart;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.appbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("");
+
+
+
+    }
+}
