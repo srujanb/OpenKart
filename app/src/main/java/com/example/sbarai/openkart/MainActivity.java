c685fab613defeabe034ad12de196fd58eef9e3e
Srujan Barai <srujan_barai@yahoo.com>
2018-01-14 22:15:32 -0500

    Creates blank activity for open orders with FAB.

--- app/src/main/java/com/example/sbarai/openkart/MainActivity.java
+++ app/src/main/java/com/example/sbarai/openkart/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.sbarai.openkart;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,8 +18,23 @@ public class MainActivity extends AppCompatActivity {
         toolbar = findViewById(R.id.appbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("");
+        toolbar.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
 
+        setOnClickListeners();
 
+    }
+
+    private void setOnClickListeners() {
+
+        View view;
+
+        view = findViewById(R.id.open_orders);
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,OpenOrders.class));
+            }
+        });
 
     }
 }
---
+++ app/src/main/java/com/example/sbarai/openkart/OpenOrders.java
@@ -0,0 +1,15 @@
+package com.example.sbarai.openkart;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OpenOrders extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_open_orders);
+
+
+    }
+}
