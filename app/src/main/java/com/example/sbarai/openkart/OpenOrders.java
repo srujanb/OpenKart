06f1b087fa69db4962d017fac18c320c51ecf39d
Srujan Barai <srujan_barai@yahoo.com>
2018-01-15 15:27:37 -0500

    Minor line reformatting

--- app/src/main/java/com/example/sbarai/openkart/OpenOrders.java
+++ app/src/main/java/com/example/sbarai/openkart/OpenOrders.java
@@ -1,15 +1,35 @@
 package com.example.sbarai.openkart;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.github.clans.fab.FloatingActionButton;
 
 public class OpenOrders extends AppCompatActivity {
+
+    FloatingActionButton createProspectOrder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_open_orders);
 
+        defineVariables();
+        setFABListeners();
+    }
 
+    private void defineVariables() {
+        createProspectOrder = findViewById(R.id.menu_item_1);
+    }
+
+    private void setFABListeners() {
+        createProspectOrder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(OpenOrders.this,CreateProspectOrder.class));
+            }
+        });
     }
 }
