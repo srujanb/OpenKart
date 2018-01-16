fe59a0d6bbd1842d150057c133d594a1fea9d60b
Dax Amin <dkamin@ncsu.edu>
2018-01-16 10:18:26 -0500

    Toolbar for Open Orders

--- app/src/main/java/com/example/sbarai/openkart/OpenOrders.java
+++ app/src/main/java/com/example/sbarai/openkart/OpenOrders.java
@@ -2,14 +2,21 @@ package com.example.sbarai.openkart;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class OpenOrders extends AppCompatActivity {
+
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_open_orders);
 
+        toolbar = findViewById(R.id.appbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("");
+        toolbar.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
 
     }
 }
