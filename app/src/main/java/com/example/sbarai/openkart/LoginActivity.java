ef215ef3a244d03b283c6f19b23d763bd6a6a17d
aayushi-21 <y13uc007@lnmiit.ac.in>
2018-01-20 17:15:38 -0500

    Improved Login and OTP Screen

--- app/src/main/java/com/example/sbarai/openkart/LoginActivity.java
+++ app/src/main/java/com/example/sbarai/openkart/LoginActivity.java
@@ -1,4 +1,4 @@
-package com.example.sbarai.openkart.Models;
+package com.example.sbarai.openkart;
 
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
@@ -7,28 +7,24 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import com.example.sbarai.openkart.R;
-import com.example.sbarai.openkart.SplashScreen;
-
 public class LoginActivity extends AppCompatActivity {
 
-    Button bt;
-    TextView textView;
-
+    Button submit;
+    TextView forgot_password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        bt = (Button)findViewById(R.id.button2);
-        bt.setOnClickListener(new View.OnClickListener() {
+        submit = (Button)findViewById(R.id.submit);
+        submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(LoginActivity.this, SplashScreen.class);
                 startActivity(intent);
             }
         });
-        textView = (TextView) findViewById(R.id.textView3);
-        textView.setOnClickListener(new View.OnClickListener() {
+        forgot_password = (TextView) findViewById(R.id.forgot_password);
+        forgot_password.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(LoginActivity.this, ForgotActivity.class);
