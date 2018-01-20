469c3506021c8af07760fa69eaadda0887fd45f8
aayushi-21 <y13uc007@lnmiit.ac.in>
2018-01-19 23:38:53 -0500

    Adding Basic SignUp and Forgot Page

--- app/src/main/java/com/example/sbarai/openkart/LoginActivity.java
+++ app/src/main/java/com/example/sbarai/openkart/LoginActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.sbarai.openkart.R;
 import com.example.sbarai.openkart.SplashScreen;
@@ -12,6 +13,7 @@ import com.example.sbarai.openkart.SplashScreen;
 public class LoginActivity extends AppCompatActivity {
 
     Button bt;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +24,14 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(LoginActivity.this, SplashScreen.class);
+                startActivity(intent);
+            }
+        });
+        textView = (TextView) findViewById(R.id.textView3);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, ForgotActivity.class);
                 startActivity(intent);
             }
         });
