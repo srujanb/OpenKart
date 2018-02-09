9872e166f84eb8368d4e3bc5dba01afb989e0758
unknown <shenee95@gmail.com>
2018-02-09 13:32:21 -0500

    phone authentatication screen

---
+++ app/src/main/java/com/example/sbarai/openkart/Utils/PhoneAuth.java
@@ -0,0 +1,15 @@
+package com.example.sbarai.openkart.Utils;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.sbarai.openkart.R;
+
+public class PhoneAuth extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_phone_auth);
+    }
+}
