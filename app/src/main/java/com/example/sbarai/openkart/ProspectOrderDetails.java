64fd09a717f9738487128f2c219c3309a24a2199
Srujan Barai <srujan_barai@yahoo.com>
2018-02-16 20:46:40 -0500

    Adds basic structure to show BottomSheet for chat option

--- app/src/main/java/com/example/sbarai/openkart/ProspectOrderDetails.java
+++ app/src/main/java/com/example/sbarai/openkart/ProspectOrderDetails.java
@@ -1,13 +1,26 @@
 package com.example.sbarai.openkart;
 
+import android.support.design.widget.BottomSheetBehavior;
+import android.support.design.widget.BottomSheetDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class ProspectOrderDetails extends AppCompatActivity {
 
+    BottomSheetDialog dialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_prospect_order_details);
+
+        openBottomSheet();
+    }
+
+    private void openBottomSheet() {
+        View view = getLayoutInflater().inflate(R.layout.bottom_sheet, null);
+        dialog = new BottomSheetDialog(this);
+        dialog.setContentView(view);
+        dialog.show();
     }
 }
