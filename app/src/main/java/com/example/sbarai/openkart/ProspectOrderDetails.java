05db460e1beab9032c6534614a0988727e959469
Srujan Barai <srujan_barai@yahoo.com>
2018-03-10 16:20:18 -0500

    [Major] Prospect order details now show all items grouped by each collaborator.

--- app/src/main/java/com/example/sbarai/openkart/ProspectOrderDetails.java
+++ app/src/main/java/com/example/sbarai/openkart/ProspectOrderDetails.java
@@ -1,14 +1,24 @@
 package com.example.sbarai.openkart;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.design.widget.BottomSheetBehavior;
 import android.support.design.widget.BottomSheetDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
+import com.example.sbarai.openkart.Models.CollaborationItem;
+import com.example.sbarai.openkart.Models.Collaborator;
 import com.example.sbarai.openkart.Models.ProspectOrder;
+import com.example.sbarai.openkart.Utils.FirebaseManager;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.ValueEventListener;
 
 public class ProspectOrderDetails extends AppCompatActivity {
 
@@ -16,7 +26,8 @@ public class ProspectOrderDetails extends AppCompatActivity {
     String POid;
     Button chatButton;
     View addItem;
-
+    LayoutInflater inflater;
+    LinearLayout listOfItems;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,14 +38,67 @@ public class ProspectOrderDetails extends AppCompatActivity {
 
 
         initVariable();
-        showAllItems();
+        getOrderDetailsAndShowItems();
     }
 
-    private void showAllItems() {
+    private void getOrderDetailsAndShowItems() {
+        FirebaseManager.getRefToSpecificProspectOrder(POid).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                ProspectOrder order = dataSnapshot.getValue(ProspectOrder.class);
+                showAllItems(order);
+            }
 
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void showAllItems(ProspectOrder order) {
+        //dummy data
+        for (String collaboratorHashKey: order.getCollaborators().keySet()){
+            Collaborator collaborator = order.getCollaborators().get(collaboratorHashKey);
+            View collaboratorView = inflater.inflate(R.layout.prospect_order_collaborator_name,null);
+            TextView collaboratorName = collaboratorView.findViewById(R.id.collaborator_name);
+            setCollaboratorName(collaboratorName,collaboratorHashKey);
+            listOfItems.addView(collaboratorView);
+            for (String itemHashKey: collaborator.getCollaborationItems().keySet()){
+                CollaborationItem item = collaborator.getCollaborationItems().get(itemHashKey);
+                View itemView = inflater.inflate(R.layout.prospect_order_item_details,null);
+                TextView itemName = itemView.findViewById(R.id.item_name);
+                itemName.setText(item.getItemName());
+                TextView itemCount = itemView.findViewById(R.id.item_count);
+                itemCount.setText(String.valueOf(item.getCount()));
+
+                listOfItems.addView(itemView);
+            }
+        }
+    }
+
+    private void setCollaboratorName(final TextView collaboratorName, String collaboratorHashKey) {
+        FirebaseManager.getRefToUserName(collaboratorHashKey).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                try {
+                    collaboratorName.setText(dataSnapshot.getValue().toString());
+                }catch (Exception e){
+                    collaboratorName.setText("Error fetching name");
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 
     private void initVariable() {
+        inflater = (LayoutInflater)getApplicationContext().getSystemService
+                (Context.LAYOUT_INFLATER_SERVICE);
+        listOfItems = findViewById(R.id.list_of_items);
         chatButton = findViewById(R.id.button_chat);
         chatButton.setOnClickListener(new View.OnClickListener() {
             @Override
